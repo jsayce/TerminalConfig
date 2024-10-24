638482b1761612ab5304bb70d2cb5dd157f4cc15
Jonathan Sayce <jsayce@sedgely.com>
2024-10-24 21:36:33 +0100

    Standardised file names

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,4 @@
 Import-Module posh-git
 Import-Module -Name Terminal-Icons
-oh-my-posh init pwsh --config "$HOME\sausage.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$HOME\OhMyPosh.json" | Invoke-Expression
 Import-Module -Name Microsoft.WinGet.CommandNotFound
